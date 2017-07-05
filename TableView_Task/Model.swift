286d55f769176a3e134a43faa7186ffa9f9c0217
Duy Bùi <duybui@Duys-Mac.local>
2017-07-05 10:00:33 +0700

    First commit

---
+++ TableView_Task/Model.swift
@@ -0,0 +1,35 @@
+//
+//  Model.swift
+//  TableView_Task
+//
+//  Created by Duy Bùi on 7/5/17.
+//  Copyright © 2017 Duy Bùi. All rights reserved.
+//
+
+import Foundation
+
+class Model {
+    let priorityName: String
+    var task = [String]()
+    
+    init(priorityName: String, task: [String]) {
+        self.priorityName = priorityName
+        self.task = task
+    }
+    
+    class func priority1() -> Model {
+        return Model(priorityName: "priority1", task: [
+            "Task1", "Task2", "Task3", "Task4", "Task5"
+            ])
+    }
+    
+    class func priority2() -> Model {
+        return Model(priorityName: "priority2", task: [
+            "Task11", "Task12", "Task13"            ])
+    }
+    
+    class func getListPriority() -> [Model]{
+        return [self.priority1(), self.priority2()]
+    }
+    
+}
--- TableView_Task/ViewController.swift
+++ TableView_Task/ViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var tableView: UITableView!
+    var priorityList: [Model] = Model.getListPriority()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tableView.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
